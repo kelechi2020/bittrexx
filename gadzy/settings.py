f0bfedad6552dcee815c4141238d236f6c795666
kelechi2020 <egbosikelechi@gmail.com>
2017-09-27 08:38:58 -0700

    settings

--- gadzy/settings.py
+++ gadzy/settings.py
@@ -62,7 +62,7 @@ INSTALLED_APPS = [
     'gadzylogin'
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
