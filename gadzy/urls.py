c83c3bbe77530b6365825b0a2fc96642b08b90a9
kelechi2020 <egbosikelechi@gmail.com>
2017-10-08 10:15:40 -0700

    shorten url

--- gadzy/urls.py
+++ gadzy/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 from gadzylogin import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.page , name='logins'),
     url(r'^bitrex/account/login/', views.page , name='login'),
 ]
